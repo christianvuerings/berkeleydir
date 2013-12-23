3087a7f6bc3f7ea369dc51979fff531ed0625103
Christian Vuerings <vueringschristian@gmail.com>
2013-12-23 03:26:06 +0100

    Add LDAP tries

--- ldap.js
+++ ldap.js
@@ -12,26 +12,38 @@ var client = ldap.createClient({
 //console.log(client);
 
 var opts = {
+  //bindDN: 'ou=people,dc=berkeley,dc=edu',
   //filter: '(&(cn=^0)(uid="61889"))'
-  filter: "(#uid=*)"
+  //filter: '(objectclass=*)'
+  //filter: '#uid=61889'
+  //filter: '((objectclass=*)(!(mail=test*)))'
 };
 
-client.search(searchBase, opts, function(err, res) {
-  console.log(err);
-  console.log(res);
+client.bind('ou=people', '', function (err) {
+  client.search(searchBase, opts, function(err, res) {
+    //console.log(err);
+    //console.log(res);
 
-  res.on('searchEntry', function (entry) {
-    console.log(entry);
-    var user = entry.object;
-    console.log(user);
-  });
-  res.on('searchReference', function(referral) {
-    console.log('referral: ' + referral.uris.join());
-  });
-  res.on('error', function(err) {
-    console.error('error: ' + err.message);
-  });
-  res.on('end', function(result) {
-    console.log('status: ' + result.status);
+    res.on('searchEntry', function (entry) {
+      //var log = JSON.stringify(entry);
+
+      // for (var i in entry) {
+      //   console.log(i, entry[i]);
+      // }
+      console.log(entry.toString());
+      var user = entry.json;
+      //console.log(user.attributes[0].vals);
+    });
+    res.on('searchReference', function(referral) {
+      console.log('referral: ' + referral.uris.join());
+    });
+    res.on('error', function(err) {
+      console.error('error: ' + err.message);
+    });
+    res.on('end', function(result) {
+      console.log('status: ' + result.status);
+    });
   });
 });
+
+// ldapsearch -H ldap://ldap.berkeley.edu -x -b 'ou=people,dc=berkeley,dc=edu'  objectclass=*
